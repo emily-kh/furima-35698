100c8397ebf10fec4a99d1529e3484f56839b5cd
emily-kh <kitaharaemiri722@gmail.com>
2021-06-09 11:49:50 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,18 +17,18 @@ class ItemsController < ApplicationController
     end
   end
   
-  def destroy
-    @item = Item.find(params[:id])
-  end
+  # def destroy
+  #   @item = Item.find(params[:id])
+  # end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
+  # def edit
+  #   @item = Item.find(params[:id])
+  # end
 
-  def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-  end
+  # def update
+  #   @item = Item.find(params[:id])
+  #   @item.update(item_params)
+  # end
 
   def show
     @item = Item.find(params[:id])
